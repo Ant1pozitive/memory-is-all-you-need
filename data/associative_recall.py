4461796547e6e5e357c69fee402e5c0d6e4693e9
Antipositive <148214981+Ant1pozitive@users.noreply.github.com>
2025-12-17 14:00:22 +0300

    Update associative_recall.py

--- data/associative_recall.py
+++ data/associative_recall.py
@@ -4,14 +4,14 @@ import torch
 
 class AssociativeRecallDataset(TaskDataset):
     """Associative recall: pairs (key-value), then query key, expect value."""
-    def __init__(self, vocab_size, num_pairs, delay_len, size=None):
-        super().__init__(vocab_size, size)
+    def __init__(self, vocab_size, num_pairs, delay_len):
+        super().__init__(vocab_size)
         self.num_pairs = num_pairs
         self.delay_len = delay_len
         self.delim = vocab_size - 1
         self.query_marker = vocab_size - 2
 
-    def __getitem__(self, idx):
+    def __next__(self):
         keys = np.random.randint(0, self.vocab_size - 2, size=(self.num_pairs,))
         values = np.random.randint(0, self.vocab_size - 2, size=(self.num_pairs,))
         query_idx = np.random.randint(0, self.num_pairs)
