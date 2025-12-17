40f01bf1b7860f3fe572ec7d7612059abbd44e9e
Antipositive <148214981+Ant1pozitive@users.noreply.github.com>
2025-12-17 14:05:09 +0300

    Update test_dataset.py

--- tests/test_dataset.py
+++ tests/test_dataset.py
@@ -1,11 +1,11 @@
 import pytest
-from src.data.copy_dataset import CopyDataset
+from data.copy_dataset import CopyDataset
 
 @pytest.fixture
 def ds():
     return CopyDataset(vocab_size=10, seq_len=3, delay_len=2)
 
 def test_getitem(ds):
-    inp, tgt = ds[0]
+    inp, tgt = next(iter(ds))
     assert len(inp) == 3 + 2 + 1 + 3
     assert (tgt[-3:] == inp[:3]).all()
