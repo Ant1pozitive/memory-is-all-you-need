5f184084eabaf4705f8cb7cfde0b956b90678114
Antipositive <148214981+Ant1pozitive@users.noreply.github.com>
2025-12-17 14:17:29 +0300

    Update copy_dataset.py

--- data/copy_dataset.py
+++ data/copy_dataset.py
@@ -1,25 +1,20 @@
-from .base import TaskDataset
-import numpy as np
+from torch.utils.data import Dataset
 import torch
+import numpy as np
 
-class CopyDataset(TaskDataset):
-    """Copy-with-delay dataset. Infinite generation."""
-    def __init__(self, vocab_size, seq_len, delay_len):
-        super().__init__(vocab_size)
+class CopyDataset(Dataset):
+    def __init__(self, vocab_size, seq_len, delay_len, size=10000):
+        self.vocab_size = vocab_size
         self.seq_len = seq_len
         self.delay_len = delay_len
         self.delim = vocab_size - 1
+        self.size = size
 
-    def __next__(self):
-        seq = np.random.randint(0, self.vocab_size - 1, size=(self.seq_len,))
-        input_seq = np.concatenate([
-            seq,
-            np.zeros(self.delay_len, dtype=np.int64),
-            np.array([self.delim], dtype=np.int64),
-            np.zeros(self.seq_len, dtype=np.int64)
-        ])
-        target = np.concatenate([
-            np.full(self.seq_len + self.delay_len + 1, -100, dtype=np.int64),
-            seq
-        ])
-        return torch.from_numpy(input_seq).long(), torch.from_numpy(target).long()
+    def __len__(self):
+        return self.size
+
+    def __getitem__(self, idx):
+        seq = np.random.randint(0, self.vocab_size - 1, size=self.seq_len)
+        input_seq = np.concatenate([seq, np.zeros(self.delay_len), [self.delim], np.zeros(self.seq_len)])
+        target = np.concatenate([np.full(self.seq_len + self.delay_len + 1, -100), seq])
+        return torch.LongTensor(input_seq), torch.LongTensor(target)
