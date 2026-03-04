488a2ed01bf80f5073a12d94e6ba5ec6d47362d7
Antipositive <148214981+Ant1pozitive@users.noreply.github.com>
2026-03-04 10:38:57 +0300

    Update associative_recall.py
    Signed-off-by: Antipositive <148214981+Ant1pozitive@users.noreply.github.com>

--- data/associative_recall.py
+++ data/associative_recall.py
@@ -1,26 +1,31 @@
-from .base import TaskDataset
+from torch.utils.data import Dataset
 import numpy as np
 import torch
+from .base import TaskDataset
 
 class AssociativeRecallDataset(TaskDataset):
-    """Associative recall: pairs (key-value), then query key, expect value."""
-    def __init__(self, vocab_size, num_pairs, delay_len):
-        super().__init__(vocab_size)
-        self.num_pairs = num_pairs
-        self.delay_len = delay_len
-        self.delim = vocab_size - 1
-        self.query_marker = vocab_size - 2
+    """Classic Associative Recall task for multi-task continual learning."""
+    def __init__(self, cfg, split: str = 'train'):
+        super().__init__(cfg.model.vocab_size)
+        self.num_pairs = cfg.task.num_pairs
+        self.delay_len = cfg.task.delay_len
+        self.delim = cfg.model.vocab_size - 1
+        self.query_marker = cfg.model.vocab_size - 2
+        self.size = 10000 if split == 'train' else 1000
 
-    def __next__(self):
+    def __len__(self):
+        return self.size
+
+    def __getitem__(self, idx):
         keys = np.random.randint(0, self.vocab_size - 2, size=(self.num_pairs,))
         values = np.random.randint(0, self.vocab_size - 2, size=(self.num_pairs,))
         query_idx = np.random.randint(0, self.num_pairs)
         input_seq = np.concatenate([
-            np.ravel(np.stack((keys, values), axis=1)),  # flatten pairs
+            np.ravel(np.stack((keys, values), axis=1)),
             np.zeros(self.delay_len, dtype=np.int64),
-            np.array([self.query_marker], dtype=np.int64),
-            np.array([keys[query_idx]], dtype=np.int64),
-            np.zeros(self.num_pairs, dtype=np.int64)  # output space
+            np.array([self.query_marker]),
+            np.array([keys[query_idx]]),
+            np.zeros(self.num_pairs, dtype=np.int64)
         ])
         target = np.concatenate([
             np.full(len(input_seq) - self.num_pairs, -100, dtype=np.int64),
